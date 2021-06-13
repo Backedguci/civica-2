74cd05f92985be5f0daf9e647e83d974af81b355
Backedguci <83763360+Backedguci@users.noreply.github.com>
2021-06-13 13:01:22 +0300

    Update poll_vote.php

--- poll_vote.php
+++ poll_vote.php
@@ -21,24 +21,10 @@ $insertvote = $yes."||".$no;
 $fp = fopen($filename,"w");
 fputs($fp,$insertvote);
 fclose($fp);
-?>
+
 
 <h2>Result:</h2>
-<table>
-<tr>
-<td>Yes:</td>
-<td><img src="poll.gif"
-width='<?php echo(100*round($yes/($no+$yes),2)); ?>'
-height='20'>
-<?php echo(100*round($yes/($no+$yes),2)); ?>%
-</td>
-</tr>
-<tr>
-<td>No:</td>
-<td><img src="poll.gif"
-width='<?php echo(100*round($no/($no+$yes),2)); ?>'
-height='20'>
-<?php echo(100*round($no/($no+$yes),2)); ?>%
-</td>
-</tr>
-</table>
+<td> Teodora
+echo ($yes)
+<td> altcineva </td>
+echo ($no)
